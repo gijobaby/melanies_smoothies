6974e42f78e7d5499a5f97a274beed64fea27a1f
Gijo Baby <164118241+gijobaby@users.noreply.github.com>
2024-03-23 19:58:56 +0800

    Update streamlit_app.py
    Add jason format

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session  #this code is for SiS
 from snowflake.snowpark.functions import col
-# New session to display fruityvice nutrition information.
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
@@ -48,4 +45,7 @@ if ingredients_list:
         st.success(success_message, icon="✅")
 
     
-
+# New session to display fruityvice nutrition information.
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.jason())
